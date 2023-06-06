cb224c215a6fe81d07fa8139de99fc30b83249d2
Jocelyn Englund <jocelyn@itsybit.se>
2023-06-06 13:29:45 +0200

    Update .gitignore for visual studio

---
+++ tests/ItsyBitseList.Tests/Wishlist.cs
@@ -0,0 +1,20 @@
+﻿namespace ItsyBitseList.Tests
+{
+    // create a Wishlist class according to the tests
+    internal class Wishlist
+    {
+        public Wishlist(string name, string owner)
+        {
+            Name = name;
+            Owner = owner;
+            Items = new List<string>();
+        }
+
+        public string Name { get; }
+        public string Owner { get; }
+        public IEnumerable<string> Items { get; internal set; }
+    }
+
+ 
+
+}
---
+++ tests/ItsyBitseList.Tests/WishlistCreationTests.cs
@@ -0,0 +1,20 @@
+﻿namespace ItsyBitseList.Tests
+{
+    public class WishlistCreationTests
+    {
+        // a test to create a new wishlist with a name and owner.
+        [Fact]
+        public void CreateWishlist()
+        {
+            // arrange
+            var wishlist = new Wishlist("My Wishlist", "John Doe");
+
+            // act
+
+            // assert
+            Assert.Equal("My Wishlist", wishlist.Name);
+            Assert.Equal("John Doe", wishlist.Owner);
+            Assert.Empty(wishlist.Items);
+        }
+    }
+}
